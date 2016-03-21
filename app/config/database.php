066e722a674f90457c627840e63fb3d9d0788701
Ryan Stoppler <rstoppler01@gmail.com>
2016-03-20 23:20:52 -0700

    Merge remote-tracking branch 'origin/master'
    url upload and clickable

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'library',
 			'username'  => 'root',
-			'password'  => '123abc',
+			'password'  => 'cvj',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
